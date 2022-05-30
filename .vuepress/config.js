82d6d089e67530450718b9aafbdc74fa0d697233
BlackHsiao <blackhsiao@qq.com>
2022-05-30 21:26:29 +0800

    do this anytime

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,23 @@
-module.exports={
-    title: 'Hello Vuepress',
-    description:'Just playing around'
+
+// 导航栏Logo 
+module.exports ={
+    themeConfig:{
+        logo : '/logo.png',
+        navber:false,  //禁用导航栏
+        
+        sidebar: [            //难道这里的路径不是相对路径？
+            '/',
+            ['/page-a','这里是侧边栏'],
+            '/page-b'
+          ],
+       
+        search: false, //禁用搜索栏
+        lastUpdated:'最新一次更新时间为：', //显示更新时间
+
+        //nextLinks:false,
+        //prevLinks:false, 自动获取当前页的上一篇和下一篇文章
+
+        smoothScroll:true, //启动页面滚动效果
+
+    }
 }
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
+---
+home: true
+
+tagline: Welcome!
+
+
+footer: 你知道二次元都活不长吗？
+---
+
 ## BlackSiao's Note
 
 *此项目为初学vuepress的尝试，想必是漏洞百出
 欢迎您的指正
-
